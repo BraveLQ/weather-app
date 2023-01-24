7069a1910ac181483e3da1c41989ea3ee0b249ac
loiq.amonbekov <amonbekov.loiq@gmail.com>
2023-01-24 21:28:11 +0500

    Bg with Glassmorphism and txt

--- resources/js/Pages/App/App.vue
+++ resources/js/Pages/App/App.vue
@@ -1,11 +1,26 @@
 <template>
-    <section class="px-12 pt-8">
-        <div>
+    <div class="bg-[url('/images/bg.png')] bg-no-repeat bg-cover bg-fixed ">
+        <section class="px-12 pt-8 text-white">
             <div>
-                <h1 class="text-2xl font-bold text-center">Weather App</h1>
+                <h1 class="text-6xl font-bold text-center">Weather App</h1>
             </div>
-        </div>
-    </section>
+
+            <div class="c-glass pb-72">
+                <div class="flex justify-between px-48">
+                    <div>
+                        <h1 class="text-5xl">NewYork</h1>
+                    </div>
+
+                    <div>
+                        <h1>ICON</h1>
+                    </div>
+
+                    <h1>81C</h1>
+
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
